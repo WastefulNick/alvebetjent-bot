7c1c40231067943609d4cd68a0f2704567fdbba3
Andreas Tvete <andreas.roed.tvete@gmail.com>
2020-12-01 16:54:29 +0100

    added user specific scores

--- commands.py
+++ commands.py
@@ -16,12 +16,29 @@ class CommandsCog(commands.Cog, name="Score Kommandoer"):
             await ctx.send('En feil har oppstått...')
             return
 
-        embed = discord.Embed(description='Poengoversikt', color=0x50bdfe)
-        for x in range(10):
-            user = score[x]
-            embed.add_field(name=f'#{x+1} ({user["display_name"]})', value=user["challenges_solved"], inline=True)
-        embed.set_footer(text=f'Etterspurt av: {ctx.message.author.name}')
-        await ctx.send(embed=embed)
+        if not args: # If no user inputted => send first 10 people in scoreboard
+            embed = discord.Embed(description='Poengoversikt', color=0x50bdfe)
+            for x in range(10):
+                user = score[x]
+                embed.add_field(name=f'#{x+1} ({user["display_name"]})', value=int(user['challenges_solved']) * 10, inline=True)
+            embed.set_footer(text=f'Etterspurt av: {ctx.message.author.name}#{ctx.message.author.discriminator}')
+            await ctx.send(embed=embed)
+        else: # If user(s) inputted => send score for specific user(s)
+            found = False
+            embed = discord.Embed(description='Poengoversikt', color=0x50bdfe)
+
+            for x in range(len(score)):
+                user = score[x]
+
+                if user['display_name'].lower() in [x.lower() for x in args]:
+                    found = True
+                    embed.add_field(name=f'#{x+1} ({user["display_name"]})', value=int(user['challenges_solved']) * 10, inline=True)
+            
+            embed.set_footer(text=f'Etterspurt av: {ctx.message.author.name}#{ctx.message.author.discriminator}')
+            await ctx.send(embed=embed)
+
+            if not found:
+                await ctx.send('Ingen bruker med dette navnet!')
 
     @commands.command()
     @commands.is_owner()

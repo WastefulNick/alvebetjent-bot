2412a300642f64c95a1a9c04110bb1107fcc85c9
Andreas Tvete <andreas.roed.tvete@gmail.com>
2020-12-02 17:14:06 +0100

    max 15 users in embed

--- commands.py
+++ commands.py
@@ -20,7 +20,7 @@ class CommandsCog(commands.Cog, name="Score Kommandoer"):
             embed = discord.Embed(title='Poengoversikt', color=0x50bdfe)
             embed_string = ''
 
-            for x in range(10):
+            for x in range(15):
                 user = score[x]
                 embed_string += f'#{x+1} {Utils().formatDisplayName(user["display_name"])} - {int(user["challenges_solved"]) * 10} poeng\n'
 
@@ -28,19 +28,11 @@ class CommandsCog(commands.Cog, name="Score Kommandoer"):
             embed.set_footer(text=f'Etterspurt av: {ctx.message.author.name}#{ctx.message.author.discriminator}')
             await ctx.send(embed=embed)
         else: # If user(s) inputted => send score for specific user(s)
-            found = False
             embed = discord.Embed(title='Poengoversikt', color=0x50bdfe)
-            embed_string = ''
 
-            for x in range(len(score)):
-                user = score[x]
+            embed_string = Utils().getScoreUsersByName(score, args)
 
-                for arg in args:
-                    if arg.lower() in user['display_name'].lower():
-                        found = True
-                        embed_string += f'#{x+1} {Utils().formatDisplayName(user["display_name"])} - {int(user["challenges_solved"]) * 10} poeng\n'
-
-            if found:
+            if embed_string:
                 embed.description = embed_string
                 embed.set_footer(text=f'Etterspurt av: {ctx.message.author.name}#{ctx.message.author.discriminator}')
                 await ctx.send(embed=embed)
--- utils.py
+++ utils.py
@@ -19,3 +19,20 @@ class Utils():
             formatted = formatted.replace(x, '💩')
         
         return formatted
+    
+    def getScoreUsersByName(self, score, input_users):
+        embed_string = ''
+        user_count = 0
+
+        for x in range(len(score)):
+            user = score[x]
+
+            for arg in input_users:
+                if arg.lower() in user['display_name'].lower():
+                    user_count += 1
+                    embed_string += f'#{x+1} {Utils().formatDisplayName(user["display_name"])} - {int(user["challenges_solved"]) * 10} poeng\n'
+                    if user_count >= 15:
+                        embed_string += '...'
+                        return embed_string
+        
+        return embed_string
